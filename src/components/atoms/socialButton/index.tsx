769760e303a3e20533dbc35ebf757a2c32d3f599
dilrong <dilrong@dilrong.com>
2021-08-16 14:38:51 +0900

    test: build

--- src/components/atoms/socialButton/index.tsx
+++ src/components/atoms/socialButton/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { socialButtonProps } from "../../types/SocialButtonProps";
+import { SocialButtonProps } from "../../types/SocialButtonProps";
 
 import "./index.css";
 
-const SocialButton: React.FC<socialButtonProps> = ({ herf, src, alt }) => (
+const SocialButton: React.FC<SocialButtonProps> = ({ herf, src, alt }) => (
   <a className="SocialButton" href={herf}>
     <img src={src} alt={alt} />
   </a>
